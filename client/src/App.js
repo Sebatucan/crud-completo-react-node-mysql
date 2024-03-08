d723b2cb614908f294b4b9e0ed29eb2912d07a5e
Sebastian <sebasfa57@gmail.com>
2024-03-08 02:33:19 -0300

    Configuración inicial
    se instalaron librarías y herramientas necesarias

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Importacion de Bootstrap 
+import Swal from 'sweetalert2' //para las alertas
+import withReactContent from 'sweetalert2-react-content' //para las alertas
+
+const
+notificaciones = withReactContent(Swal);
+
+function App() {
+  return (
+    <div className="App">
+     
+<h1>kuakkk</h1>
+
+
+     
+    </div>
+  );
+}
+
+export default App;
